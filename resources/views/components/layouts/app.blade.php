5cf0074f1f1c7a5be68531640b651f38838bd1a8
davidMorekwa <davenyamongo16@gmail.com>
2024-04-25 22:53:01 +0300

    Updates

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @livewireStyles
         <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
         @vite('resources/css/app.css')
         <title>SASRA VMS</title>
@@ -11,6 +12,7 @@
         {{ $slot }}
         {{-- @yield('content') --}}
         
+        @livewireScripts
     </body>
     <x-footer />
 </html>
--- resources/views/livewire/dashboard/other.blade.php
+++ resources/views/livewire/dashboard/other.blade.php
@@ -1,39 +1,40 @@
-    <div>
-        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-{{-- ADD USER --}}
-            <div class="mt-10 sm:mt-0">
-                @livewire('profile.add-user')
-            </div>
-            <x-section-border />
-
-                <x-add-visit-purpose />
-
-            <x-section-border />
-{{-- SHOW LOGS --}}
-            <div class="mt-10 sm:mt-0">
-                <x-action-section>
-                    <x-slot name="title">
-                        {{ __('Logs') }}
-                    </x-slot>
-                
-                    <x-slot name="description">
-                        {{ __('Show system logs') }}
-                    </x-slot>
-                
-                    <x-slot name="content">
-                        <div class="max-w-xl text-sm text-gray-600">
-                            {{ __('System logs are records of events that happen on a computer system, such as errors, warnings, and information about
-                            current operations') }}
-                        </div>
-                
-                        <div class="mt-5">
-                            <a href="{{ route('show.logs') }}">
-                                <x-danger-button class="border p-2 rounded-lg bg-gray-700 text-white">Show Logs</x-danger-button>
-                            </a>
-                        </div>
-                    </x-slot>
-                </x-action-section>
-            </div>
-            
+<div>
+    <div class="max-w-7xl mx-auto py-10 my_smpx-6 lg:px-8">
+        {{-- ADD USER --}}
+        <div class="mt-10 my_smmt-0">
+            @livewire('profile.add-user')
         </div>
+        <x-section-border />
+
+        <x-add-visit-purpose />
+
+        <x-section-border />
+        {{-- SHOW LOGS --}}
+        <div class="mt-10 my_smmt-0">
+            <x-action-section>
+                <x-slot name="title">
+                    {{ __('Logs') }}
+                </x-slot>
+
+                <x-slot name="description">
+                    {{ __('Show system logs') }}
+                </x-slot>
+
+                <x-slot name="content">
+                    <div class="max-w-xl text-sm text-gray-600">
+                        {{ __('System logs are records of events that happen on a computer system, such as errors, warnings, and information about
+                            current operations') }}
+                    </div>
+
+                    <div class="mt-5">
+                        <a href="{{ route('show.logs') }}">
+                            <x-danger-button class="border p-2 rounded-lg bg-gray-700 text-white">Show Logs
+                            </x-danger-button>
+                        </a>
+                    </div>
+                </x-slot>
+            </x-action-section>
+        </div>
+
     </div>
+</div>
